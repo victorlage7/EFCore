70c53a62e159c9962f50e102c454b1774a6080e7
Paulo Victor <victorlage7@gmail.com>
2020-03-24 16:32:20 -0300

    Interface ProdutoDAo e adptação para operar das duas formas.

---
+++ Alura.Loja.Testes.ConsoleApp/ProdutoDAOEntity.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Alura.Loja.Testes.ConsoleApp
+{
+    class ProdutoDAOEntity : IProdutoDAO, IDisposable
+    {
+        private LojaContext contexto;
+
+        public void Adicionar(Produto p)
+        {
+            contexto.Produtos.Add(p);
+            contexto.SaveChanges();
+        }
+
+        public void Atualizar(Produto p)
+        {
+            contexto.Produtos.Update(p);
+            contexto.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Produto> Produtos()
+        {
+            return contexto.Produtos.ToList();
+        }
+
+        public void Remover(Produto p)
+        {
+            contexto.Produtos.Remove(p);
+            contexto.SaveChanges();
+        }
+    }
+}
--- Alura.Loja.Testes.ConsoleApp/Program.cs
+++ Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -10,20 +10,64 @@ namespace Alura.Loja.Testes.ConsoleApp
     {
         static void Main(string[] args)
         {
-            GravarUsandoAdoNet();
+            //GravarUsandoAdoNet();
+            // GravarUsandoEntity();
+            //RecuperarProdutos();
+            ExcluirProdutos();
         }
 
-        private static void GravarUsandoAdoNet()
+
+        private static void AtualizaProduto()
+        {
+            // inclui um produto
+
+            // atualiza o produto
+            using (var repo = new ProdutoDAOEntity())
+            {
+                Produto primeiro = repo.Produtos().First();
+                primeiro.Nome = "Cassino Royale - Editado";
+                repo.Atualizar(primeiro);
+            }
+            RecuperarProdutos();
+        }
+
+        private static void ExcluirProdutos()
+        {
+            using (var repo = new ProdutoDAOEntity())
+            {
+                IList<Produto> produtos = repo.Produtos();
+                foreach (var item in produtos)
+                {
+                    repo.Remover(item);
+                }
+            }
+        }
+
+        private static void RecuperarProdutos()
+        {
+            using (var repo = new ProdutoDAOEntity())
+            {
+                IList<Produto> produtos = repo.Produtos();
+                Console.WriteLine("Foram encontrados {0} produto(s).", produtos.Count);
+                foreach (var item in produtos)
+                {
+                    Console.WriteLine(item.Nome);
+                }
+            }
+        }
+
+        private static void GravarUsandoEntity()
         {
             Produto p = new Produto();
-            p.Nome = "Harry Potter e a Ordem da Fênix";
-            p.Categoria = "Livros";
+            p.Nome = "Cassino Royale";
+            p.Categoria = "Filmes";
             p.Preco = 19.89;
 
-            using (var repo = new ProdutoDAO())
+            using (var repo = new ProdutoDAOEntity())
             {
                 repo.Adicionar(p);
             }
         }
+
     }
 }
